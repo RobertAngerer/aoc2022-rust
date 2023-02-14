0812c04fb79bba67c2f896d6dabd5f77818899b3
Robert Angerer <robert.angerer@outlook.com>
2023-02-14 21:53:06 +0100

    Day 06 part 1 solved, part 2 started

---
+++ day06/src/main.rs
@@ -0,0 +1,84 @@
+use std::fs::File;
+use std::{fs, io};
+use std::collections::HashSet;
+use std::io::BufRead;
+use std::path::Path;
+
+fn main() {
+    let input = read_file();
+    solve01(&input);
+    solve02(&input);
+}
+
+fn solve02(input: &str) {
+    let mut first_14 = &input[1520..];
+    println!("{:?}", first_14);
+    let mut counter = 1534;
+    let start_string = &input[13..];
+    for i in start_string.chars() {
+        println!("{:?}, {:?}", first_14, i);
+        if first_14.contains(i) {
+            first_14 = &input[counter-12..counter+1];
+            counter +=1
+        }
+        else if contains_duplicates(first_14){
+            first_14 = &input[counter-13..counter+1];
+            counter +=1
+        }
+        else {
+            println!("{:?}, {:?}", first_14, i);
+            println!("{:?}", counter + 1);
+            break
+        }
+    }
+}
+
+fn solve01(input: &str) {
+    let mut first_three = &input[..3];
+    println!("{:?}", first_three);
+    let mut counter = 3;
+    let start_string = &input[3..];
+    for i in start_string.chars() {
+        println!("{:?}, {:?}", first_three, i);
+        if first_three.contains(i) {
+            first_three = &input[counter-2..counter+1];
+            counter +=1
+        }
+        else if contains_duplicates(first_three){
+            first_three = &input[counter-2..counter+1];
+            counter +=1
+        }
+        else {
+            println!("{:?}, {:?}", first_three, i);
+            println!("{:?}", counter + 1);
+            break
+        }
+    }
+
+}
+
+
+fn contains_duplicates(input: &str) -> bool {
+    let mut set = HashSet::new();
+    for c in input.chars() {
+        if set.contains(&c) {
+            return true;
+        }
+        set.insert(c);
+    }
+    false
+}
+
+
+
+fn read_file() -> String {
+    let contents = fs::read_to_string("input06.txt").expect("should");
+    println!("With text:\n{contents}");
+    return contents
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
